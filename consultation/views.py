cfc421a0c589787138df8708ff9d5768b2653b74
ziko278 <odekeziko@gmail.com>
2025-10-24 12:02:29 +0100

    l

--- consultation/views.py
+++ consultation/views.py
@@ -3332,7 +3332,7 @@ def patient_history_view(request, patient_id):
     )
 
     # Paginate the results, 3 per page
-    paginator = Paginator(all_consultations, 5)
+    paginator = Paginator(all_consultations, 6)
     page_obj = paginator.get_page(1)  # Get the first page
 
     context = {
